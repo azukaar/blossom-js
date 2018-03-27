d464352ca4ebf2c3cca792eb9784ef9f4efe3842
Yann Stepienik <azukaar@gmail.com>
2018-03-27 20:34:27 +0100

    src to modules

--- packages/blossom-js/webpack.config.js
+++ packages/blossom-js/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './modules/index.js',
   output: {
     path: path.resolve(__dirname, 'umd'),
     filename: 'index.js',
