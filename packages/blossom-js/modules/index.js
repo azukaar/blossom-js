15051bc21ac9640928a1279a5043edbf6dfa384a
Yann Stepienik <azukaar@gmail.com>
2018-05-09 22:05:45 +0100

    Add polyfills

--- packages/blossom-js/modules/index.js
+++ packages/blossom-js/modules/index.js
@@ -1,3 +1,4 @@
+import '@webcomponents/webcomponentsjs/bundles/webcomponents-ce.js';
 import { BlossomComponent, BlossomRegister, BlossomResolveScope, BlossomInterpolate } from 'blossom-js-custom-element';
 import BlossomRouter from 'blossom-js-router';
 import 'blossom-js-html';
