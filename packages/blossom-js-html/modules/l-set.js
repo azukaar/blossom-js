334ed603b4638e608223c7441cec802318db891a
Yann Stepienik <azukaar@gmail.com>
2018-05-11 22:39:06 +0100

    l-set fix

--- packages/blossom-js-html/modules/l-set.js
+++ packages/blossom-js-html/modules/l-set.js
@@ -4,7 +4,7 @@ class SetComponent extends BlossomComponent {
   render() {
     Object.keys(this.props).forEach((name) => {
       if (!name.match(/^has_set_/)) {
-        const hasValue = (typeof this.props[name] === 'function') ? this.props[name].toString() : this.props[name];
+        const hasValue = BlossomSerialise(this.props[name]);
 
         if (!this.hasAttribute(`has_set_${name}`) || this.getAttribute(`has_set_${name}`) !== BlossomSerialise(hasValue)) {
           if (this.ctx[name]) {
