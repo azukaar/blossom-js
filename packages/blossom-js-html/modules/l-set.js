d32e4d9a6e82162ca55860098bdab7acbdefdce7
Yann Stepienik <azukaar@gmail.com>
2018-05-29 17:18:21 +0100

    fix issue with l-set double serialisation

--- packages/blossom-js-html/modules/l-set.js
+++ packages/blossom-js-html/modules/l-set.js
@@ -4,12 +4,11 @@ class SetComponent extends BlossomComponent {
   render() {
     Object.keys(this.props).forEach((name) => {
       if (!name.match(/^has_set_/)) {
-        const hasValue = BlossomSerialise(this.props[name]);
+        const setValue = BlossomSerialise(this.props[name]);
 
-        if (!this.hasAttribute(`has_set_${name}`) || this.getAttribute(`has_set_${name}`) !== BlossomSerialise(hasValue)) {
-          const toSet = BlossomSerialise(this.props[name]);
-          this.setAttribute(`has_set_${name}`, toSet);
-          this.ctx[name] = toSet;
+        if (!this.hasAttribute(`has_set_${name}`) || this.getAttribute(`has_set_${name}`) !== setValue) {
+          this.setAttribute(`has_set_${name}`, setValue);
+          this.ctx[name] = setValue;
         }
       }
     });
