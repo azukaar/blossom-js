baf320fc90d424dc2c9de2a129353c5127cfa079
Yann Stepienik <azukaar@gmail.com>
2018-06-06 02:08:29 +0100

    docs fix

--- bs.js
+++ bs.js
@@ -9,7 +9,7 @@ browserSync.init({
         middleware: [ historyApiFallback({
           rewrites: [
             { from: /(css|jpg|png|gif|js|html)$/, to: (context) => context.parsedUrl.pathname },
-            { from: /\/doc/, to: '/doc/index.html' },
+            { from: /\/docs/, to: '/docs/index.html' },
             { from: /\/examples\/ToDo/, to: '/examples/ToDo/index.html' },
             { from: /\/examples\/weather/, to: '/examples/weather/index.html' },
           ]
