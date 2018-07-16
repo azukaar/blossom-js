9549f3dbbe5d55acb14a8ca00f19f1b2b92ee45e
Yann Stepienik <azukaar@gmail.com>
2018-07-16 20:41:10 +0100

    get rid of monorepo

--- modules/index.js
+++ modules/index.js
@@ -1,8 +1,10 @@
+import '@webcomponents/webcomponentsjs/bundles/webcomponents-ce';
 import { BlossomReady, register, getCtx, interpolate } from './utils';
 import { BlossomElement } from './convertElement';
 import createElement from './jsx';
 import Component from './Component';
 import { serialise, deserialise } from './serialise';
+
 
 export {
   serialise,
